2e5e08d68e56b25edee4d7452a2503883de68350
Ethan Luong <eluong41@gmail.com>
2023-03-27 16:54:20 +0200

    Ajout des thèmes Etoile Blanche et Matière Noire

--- src/HubPrincipal.js
+++ src/HubPrincipal.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Accueil from "./Accueil.js";
 import Connexion from "./Connexion.js";
 import Entete from "./Entete.js";
@@ -8,6 +8,31 @@ import Inscription from "./Inscription.js";
 function HubPrincipal(props){
     const [pageCourante, setPageCourante] = useState(props.pageCourante);
     const [statutConnexion, setStatutConnexion] = useState(props.statutConnexion);
+    const [theme, setTheme] = useState("etoile-blanche");
+
+    useEffect(() => {
+        let root = document.documentElement;
+        if (theme == "etoile-blanche") {
+            root.style.setProperty("--ami-bg-color", "rgba(44,43,74,1)");
+            root.style.setProperty("--infos-bg-color", "rgba(44,43,74,1)");
+            root.style.setProperty("--main-bg-color", "rgba(255,255,255,1)");
+            root.style.setProperty("--menu-bg-color", "rgba(44,43,74,1)");
+            root.style.setProperty("--message-bg-color", "rgba(255,255,255,1)");
+            root.style.setProperty("--message-bordure-color", "rgba(0,0,0,0.25)");
+            root.style.setProperty("--soustexte-color", "rgba(0,0,0,0.25)");
+            root.style.setProperty("--texte-color", "rgba(0,0,0,1)");
+        }
+        else if (theme == "matiere-noire") {
+            root.style.setProperty("--ami-bg-color", "rgba(23,21,33,1)");
+            root.style.setProperty("--infos-bg-color", "rgba(23,21,33,1)");
+            root.style.setProperty("--main-bg-color", "rgba(44,43,74,1)");
+            root.style.setProperty("--menu-bg-color", "rgba(23,21,33,0.5)");
+            root.style.setProperty("--message-bg-color", "rgba(23,21,33,0.5)");
+            root.style.setProperty("--message-bordure-color", "rgba(140,107,203,0.25)");
+            root.style.setProperty("--soustexte-color", "rgba(255,255,255,0.25)");
+            root.style.setProperty("--texte-color", "rgba(255,255,255,1)");
+        }
+    }, [theme]);
 
     const seConnecter = () => {
         setStatutConnexion(true);
