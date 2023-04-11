0e9c27d32648c594ca9ac576cd9fb95abe9b8a95
Ethan2541 <eluong41@gmail.com>
2023-04-11 02:00:16 +0200

    Légers réajustements du CSS des utilisateurs

--- client/src/components/Users/User.js
+++ client/src/components/Users/User.js
@@ -5,13 +5,13 @@ import './styles/user.css';
 function User(props) {
     const navigate = useNavigate()
     return(
-        <li id={ props.userId } className='user' onClick={ (evt) => navigate(`${props.userLogin}`)}>
+        <div className='user' onClick={ (evt) => navigate(`${props.userLogin}`)}>
             <img draggable='false' src={ props.avatar } alt={ 'Avatar de ' + props.userLogin } />
             <div className='user-metadata'>
                 <h3>{ props.userLogin }</h3>
                 <h4>{ props.type === 'follower' ? 'Vous suit' : 'Suivi' } depuis le { props.date }</h4>
             </div>
-        </li>
+        </div>
     );
 }
 
