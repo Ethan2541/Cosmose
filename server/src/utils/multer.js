518d47a7ce0556889c10a15979421b43fc264af1
Ethan Luong <eluong41@gmail.com>
2023-04-19 16:23:30 +0200

    Changement taille max. images

--- server/src/utils/multer.js
+++ server/src/utils/multer.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = multer({
     storage: multer.diskStorage({}),
-    limits: { fileSize: 0.5 * 1024 * 1024 },
+    limits: { fileSize: 2 * 1024 * 1024 },
     fileFilter: (req, file, cb) => {
         let ext = path.extname(file.originalname);
         if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
