fdec8344c21afb0ac882bec9aac6309e9276500a
Ethan Luong <eluong41@gmail.com>
2023-04-19 20:23:06 +0200

    Correction bug likes

--- server/src/controllers/likes.js
+++ server/src/controllers/likes.js
@@ -4,50 +4,67 @@ const mongo = require('mongodb');
 
 // Add like
 exports.addLike = (req, res, next) => {
-    db.collection('likes').insertOne(
-        {
-            userLogin: req.body.userLogin,
-            messageId: new mongo.ObjectId(req.body.messageId),
-            date: new Date()
-        })
-        .then(valid => {
-            // Invalid insertion
-            if (!valid) {
-                return res.status(400).json({ error: 'Could not like' })
+    // Check user has already liked the message
+    db.collection('likes').findOne({ userLogin: req.body.userLogin, messageId: new mongo.ObjectId(req.body.messageId) })
+        .then(like => {
+            if (like) {
+                return res.status(400).json({ error: 'Already liked' })
             }
-            // Increase the message's likes counter
-            db.collection('messages').updateOne({ _id: new mongo.ObjectId(req.body.messageId) }, { $inc: { likes: 1 } })
-                .then(result => {
-                    if (!result) {
-                        return res.status(400).json({ error: 'Could not increase the number of likes' })
-                    }
-                    res.status(204).json();
+            // Creation
+            db.collection('likes').insertOne(
+                {
+                    userLogin: req.body.userLogin,
+                    messageId: new mongo.ObjectId(req.body.messageId),
+                    date: new Date()
                 })
-                .catch(err => res.status(500).json({ error: 'Could not increase the number of likes' }));
+                .then(valid => {
+                    // Invalid insertion
+                    if (!valid) {
+                        return res.status(400).json({ error: 'Could not like' })
+                    }
+                    // Increase the message's likes counter
+                    db.collection('messages').updateOne({ _id: new mongo.ObjectId(req.body.messageId) }, { $inc: { likes: 1 } })
+                        .then(result => {
+                            if (!result) {
+                                return res.status(400).json({ error: 'Could not increase the number of likes' })
+                            }
+                            res.status(204).json();
+                        })
+                        .catch(err => res.status(500).json({ error: 'Could not increase the number of likes' }));
+                })
+                .catch(err => res.status(500).json({ error: 'Could not like' }));
         })
-        .catch(err => res.status(500).json({ error: 'Could not like' }));
+        .catch(err => res.status(500).json({ error: 'Could not check if user has already liked the message' }))
 }
 
 
 // Delete like
 exports.deleteLike = (req, res, next) => {
-    db.collection('likes').deleteOne({ userLogin: req.query.userLogin, messageId: new mongo.ObjectId(req.query.messageId) })
-        .then(valid => {
-            // Invalid deletion
-            if (!valid) {
-                return res.status(400).json({ error: 'Could not unlike' })
+    db.collection('likes').findOne({ userLogin: req.query.userLogin, messageId: new mongo.ObjectId(req.query.messageId) })
+        .then(like => {
+            if (!like) {
+                return res.status(400).json({ error: 'Not liked' })
             }
-            // Decrease the message's likes counter
-            db.collection('messages').updateOne({ _id: new mongo.ObjectId(req.query.messageId) }, { $inc: { likes: -1 } })
-                .then(result => {
-                    if (!result) {
-                        return res.status(400).json({ error: 'Could not decrease the number of likes' })
-                    }
-                    res.status(204).json();
-                })
-                .catch(err => res.status(500).json({ error: 'Could not decrease the number of likes' }));
+            // Deletion
+            db.collection('likes').deleteOne({ userLogin: req.query.userLogin, messageId: new mongo.ObjectId(req.query.messageId) })
+            .then(valid => {
+                // Invalid deletion
+                if (!valid) {
+                    return res.status(400).json({ error: 'Could not unlike' })
+                }
+                // Decrease the message's likes counter
+                db.collection('messages').updateOne({ _id: new mongo.ObjectId(req.query.messageId) }, { $inc: { likes: -1 } })
+                    .then(result => {
+                        if (!result) {
+                            return res.status(400).json({ error: 'Could not decrease the number of likes' })
+                        }
+                        res.status(204).json();
+                    })
+                    .catch(err => res.status(500).json({ error: 'Could not decrease the number of likes' }));
+            })
+            .catch(err => res.status(500).json({ error: 'Could not unlike' }));
         })
-        .catch(err => res.status(500).json({ error: 'Could not unlike' }));
+        .catch(err => res.status(500).json({ error: 'Could not check if user has liked the message' }))
 }
 
 
